39a13052bff7e437ebaed3bf185387c5770766cb
Elo-Maria Roots <elo-maria.roots@khk.ee>
2018-10-10 16:31:30 +0300

    faas3

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 let numInput = document.querySelector('#numInput');
 let factOutput = document.querySelector('#factOutput');
+let incInput = document.querySelector('#incInput');
 
 const getFact = () => {
+    factOutput.innerHTML = ''
     let number = numInput.value;
-}
+    let inc = incInput.value;
+    let baseUrl = 'http://numbersapi.com/'
+    let url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
+
   
-if (number != '') }
-fech('http://numbersapi.com/'+number)
-    .then(response => response.text())
-console.log(response.data);
+if (number != '' && inc != '') 
+ axios.get(url)
+    .then(response => {
+        for (let item in response.data) {
+            let li = document.createElement('li');
 
-    .then(data => {
-    facktOutput.innerText = data;
-})
-.catch(error => {
-    console.log(error);
-})
-
+            factOutput.appendChild(li);
+            li.innerText = response.data[item];
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
 
 // Listening to events
 numInput.addEventListener('input', getFact);
+incInput.addEventListener('input', getFact);
     
